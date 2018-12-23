d2929da6607d85476e1c033b4e435efdcc6d101d
Adrit <axb5095@gmail.com>
2018-12-23 11:07:33 -0500

    committing changes

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
+mongoose.connect(MONGODB_URI);
 console.log("Connected to the database")
 // Routes
 
